404eda3f52f0d7140027e2230fbce70846a2d2b7
cmchicas <carlos.chicas@gmail.com>
2021-06-04 18:20:29 -0600

    first commit

---
+++ src/app/components/employee/employee.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {EmployeeService} from '../../services/employee.service'
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private employeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    console.log(this.employeService.getEmployees());
+  }
+
+}
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  URL_API = 'http://localhost:3000/api/employees'
+
+  constructor() { }
+
+  getEmployees(){
+    
+    return [];
+  }
+
+}
